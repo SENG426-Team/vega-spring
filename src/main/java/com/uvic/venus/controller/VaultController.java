651d1485f187fde041d8c051f54d7a72fad4b446
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-06-27 11:24:52 -0700

    Added file parameter to `addSecretToVault`

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -2,14 +2,13 @@ package com.uvic.venus.controller;
 
 import com.uvic.venus.repository.SecretElementDAO;
 import com.uvic.venus.repository.SharedSecretDAO;
+import com.uvic.venus.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
 import com.uvic.venus.model.Secrets;
 import com.uvic.venus.model.SharedSecrets;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.sql.DataSource;
 
@@ -22,7 +21,7 @@ public class VaultController {
     private static int common_shared_id = 0;
 
     @Autowired
-    private DataSource dataSource;
+    DataSource dataSource;
 
     @Autowired
     SecretElementDAO secretElementDAO;
@@ -30,12 +29,16 @@ public class VaultController {
     @Autowired
     SharedSecretDAO sharedSecretDAO;
 
+    @Autowired
+    StorageService storageService;
 
 
     // add item to vault - post
     @RequestMapping(value = "/add_secret", method = RequestMethod.POST)
-    public ResponseEntity<?> addSecretToVault(@RequestBody Secrets secrets) {
+    public ResponseEntity<?> addSecretToVault(@RequestBody Secrets secrets, @RequestParam("file") MultipartFile file) {
         System.out.println("Entered into addSecretToVault");
+
+        storageService.store(file);
 
         Secrets secret = new Secrets(common_secret_id++, secrets.getUsername(), secrets.getDate_created(), secrets.getSecret());
         System.out.println(secrets);
